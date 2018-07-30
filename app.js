42e390aeda028a1e16af29ae9aca81593425aa12
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2018-07-30 19:05:46 +0200

    added testdata and url

--- app.js
+++ app.js
@@ -1,43 +1,63 @@
-"use strict";
+(function() {
 
-$('#button').on('click', function() {
+    "use strict";
 
-    // html seite mit button json schicken
-    // zahlen
-    // bools null
-    // objekt array
-    // array aus strings
-    // array aus objekten
+    $('#button').on('click', function() {
 
-    var data = {
-        number: 0,
-        bool: true,
-        string: 'test',
-        date: new Date(),
-        object: {
-            test: '!"§$%&/()=?`*Ä_:;,.-_.,+~´`}][{³²°|<>'
-        },
-        stringArray: ["test1", "test2"],
-        objectArray: [
-            {
-                number: 0,
-                bool: true,
-            },{
-                number: 0,
-                bool: true,
+        // html seite mit button json schicken
+        // zahlen
+        // bools null
+        // objekt array
+        // array aus strings
+        // array aus objekten
+
+        var data = {
+            number: 0,
+            bool: true,
+            string: 'test',
+            null: null,
+            undefined: undefined,
+            symbol: Symbol(),
+            date: new Date(),
+            object: {
+                test: '!"§$%&/()=?`*Ä_:;,.-_.,+~´`}][{³²°|<>'
+            },
+            stringArray: ["test1", "test2"],
+            objectArray: [
+                {
+                    number: 0,
+                    bool: true,
+                },{
+                    number: 0,
+                    bool: true,
+                }
+            ],
+            arrayArray: [
+                [0,1,2,3],
+                ['a','b','c']
+            ],
+            mixedArray: [
+                [0,1,2,3],
+                {test:'test3'},
+                ['a','b','c']
+            ]
+        };
+
+        // zum überprüfen wie die Daten aussahen vor dem losschicken
+        console.log(data);
+        data = JSON.stringify(data);
+        console.log('json data: ' + data);
+
+        $.ajax({
+            url: '/endpoint.fcgi',
+            data: data,
+            method: 'POST',
+            success: function(response) {
+                console.log('response: ' + response);
+            },
+            error: function(error) {
+                console.log('error: ' + error.state());
             }
-        ]
-    };
-
-    $.ajax({
-        url: 'url hier einfügen',
-        data: JSON.stringify(data),
-        method: 'POST',
-        success: function(response) {
-           console.log(response);
-        },
-        error: function(er) {
-            console.log(er);
-        }
+        });
     });
-});
+})();
